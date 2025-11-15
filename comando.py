7e8ed3114b9efd0ec956c4191561aa6661045d21
fl4v10pessoa <100494569+fl4v10pessoa@users.noreply.github.com>
2025-11-15 07:01:43 -0300

    Update comando.py

--- comando.py
+++ comando.py
@@ -1,129 +1,112 @@
-# VERSION: 1.2
-# AUTHORS: mauricci
+# VERSION: 1.22
+# AUTHORS: nindogo
+# CONTRIBUTORS: Diego de las Heras (ngosang@hotmail.es)
 
+import http.client
 import re
-from datetime import datetime
+import sys
+import urllib.error
+import urllib.parse
+import urllib.request
+from datetime import datetime, timedelta
 from html.parser import HTMLParser
-from typing import Any, Dict, List, Mapping, Tuple, Union
-from urllib.parse import unquote
+from typing import Callable, Dict, List, Mapping, Match, Tuple, Union
 
 from helpers import retrieve_url
 from novaprinter import prettyPrinter
 
 
 class comando:
-    url = 'https://redetorrent.com/'
-    name = 'Comando'
-    supported_categories = {'all': '0'}
+    name = "Comando"
+    url = 'https://comando.la/'
+    supported_categories = {'all': 'all', 'tv': 'tv'}
 
-    class MyHTMLParser(HTMLParser):
+    class MyHtmlParser(HTMLParser):
+        A, TD, TR, TABLE = ('a', 'td', 'tr', 'table')
 
+        """ Sub-class for parsing results """
         def __init__(self, url: str) -> None:
             HTMLParser.__init__(self)
             self.url = url
-            self.insideResults = False
-            self.insideDataDiv = False
-            self.pageComplete = False
-            self.spanCount = -1
-            self.infoMap = {
-                "name": 0,
-                "torrLink": 0,
-                "seeds": 2,
-                "leech": 3,
-                "pub_date": 4,
-                "size": 5,
-            }
-            self.fullResData: List[object] = []
-            self.pageRes: List[object] = []
-            self.singleResData = self.get_single_data()
 
-        def get_single_data(self) -> Dict[str, Any]:
-            return {
-                'name': '-1',
-                'seeds': '-1',
-                'leech': '-1',
-                'size': '-1',
-                'link': '-1',
-                'desc_link': '-1',
-                'engine_url': self.url,
-                'pub_date': '-1',
+            now = datetime.now()
+            self.date_parsers: Mapping[str, Callable[[Match[str]], datetime]] = {
+                r"(\d+)h\s+(\d+)m": lambda m: now - timedelta(hours=int(m[1]), minutes=int(m[2])),
+                r"(\d+)d\s+(\d+)h": lambda m: now - timedelta(days=int(m[1]), hours=int(m[2])),
+                r"(\d+)\s+weeks?": lambda m: now - timedelta(weeks=int(m[1])),
+                r"(\d+)\s+mo": lambda m: now - timedelta(days=int(m[1]) * 30),
+                r"(\d+)\s+years?": lambda m: now - timedelta(days=int(m[1]) * 365),
             }
+            self.in_table_row = False
+            self.current_item: Dict[str, object] = {}
 
         def handle_starttag(self, tag: str, attrs: List[Tuple[str, Union[str, None]]]) -> None:
             def getStr(d: Mapping[str, Union[str, None]], key: str) -> str:
                 value = d.get(key, '')
                 return value if value is not None else ''
 
-            attributes = dict(attrs)
-            if tag == 'div' and 'nav' in getStr(attributes, 'id'):
-                self.pageComplete = True
-            if tag == 'div' and attributes.get('id', '') == 'similarfiles':
-                self.insideResults = True
-            if tag == 'div' and self.insideResults and 'gac_bb' not in getStr(attributes, 'class'):
-                self.insideDataDiv = True
-            elif tag == 'span' and self.insideDataDiv and 'verified' != attributes.get('title', ''):
-                self.spanCount += 1
-            if self.insideDataDiv and tag == 'a' and len(attrs) > 0:
-                if self.infoMap['torrLink'] == self.spanCount and 'href' in attributes:
-                    self.singleResData['link'] = self.url + getStr(attributes, 'href')
-                if self.infoMap['name'] == self.spanCount and 'href' in attributes:
-                    self.singleResData['desc_link'] = self.url + getStr(attributes, 'href')
+            params = dict(attrs)
 
-        def handle_endtag(self, tag: str) -> None:
-            if not self.pageComplete:
-                if tag == 'div':
-                    self.insideDataDiv = False
-                    self.spanCount = -1
-                    if len(self.singleResData) > 0:
-                        # ignore trash stuff
-                        if self.singleResData['name'] != '-1' \
-                                and self.singleResData['size'] != '-1' \
-                                and self.singleResData['name'].lower() != 'nome':
-                            # ignore those with link and desc_link equals to -1
-                            if self.singleResData['desc_link'] != '-1' \
-                                    or self.singleResData['link'] != '-1':
-                                try:
-                                    date_string = self.singleResData['pub_date']
-                                    date = datetime.strptime(date_string, '%Y-%m-%d %H:%M:%S')
-                                    self.singleResData['pub_date'] = int(date.timestamp())
-                                except Exception:  # pylint: disable=broad-exception-caught
-                                    pass
-                                try:
-                                    prettyPrinter(self.singleResData)  # type: ignore[arg-type] # refactor later
-                                except Exception:  # pylint: disable=broad-exception-caught
-                                    print(self.singleResData)
-                                self.pageRes.append(self.singleResData)
-                                self.fullResData.append(self.singleResData)
-                        self.singleResData = self.get_single_data()
+            if (params.get('class') == 'forum_header_border'
+                    and params.get('name') == 'hover'):
+                self.in_table_row = True
+                self.current_item = {}
+                self.current_item['seeds'] = -1
+                self.current_item['leech'] = -1
+                self.current_item['size'] = -1
+                self.current_item['engine_url'] = self.url
+                self.current_item['pub_date'] = -1
+
+            if (tag == self.A
+                    and self.in_table_row and params.get('class') == 'magnet'):
+                self.current_item['link'] = params.get('href')
+
+            if (tag == self.A
+                    and self.in_table_row and params.get('class') == 'epinfo'):
+                self.current_item['desc_link'] = self.url + getStr(params, 'href')
+                self.current_item['name'] = getStr(params, 'title').split(' (')[0]
 
         def handle_data(self, data: str) -> None:
-            if self.insideDataDiv:
-                for key, val in self.infoMap.items():
-                    if self.spanCount == val:
-                        curr_key = key
-                        if curr_key in self.singleResData and data.strip() != '':
-                            if self.singleResData[curr_key] == '-1':
-                                self.singleResData[curr_key] = data.strip()
-                            elif curr_key != 'name':
-                                self.singleResData[curr_key] += data.strip()
+            data = data.replace(',', '')
+            if (self.in_table_row
+                    and (data.endswith(' KB') or data.endswith(' MB') or data.endswith(' GB'))):
+                self.current_item['size'] = data
+
+            elif self.in_table_row and data.isnumeric():
+                self.current_item['seeds'] = int(data)
+
+            elif self.in_table_row:  # Check for a relative time
+                for pattern, calc in self.date_parsers.items():
+                    m = re.match(pattern, data)
+                    if m:
+                        self.current_item["pub_date"] = int(calc(m).timestamp())
+                        break
+
+        def handle_endtag(self, tag: str) -> None:
+            if self.in_table_row and tag == self.TR:
+                prettyPrinter(self.current_item)  # type: ignore[arg-type] # refactor later
+                self.in_table_row = False
+
+    def do_query(self, what: str) -> str:
+        url = f"{self.url}/search/{what.replace('%20', '-')}"
+        data = b"layout=def_wlinks"
+        try:
+            return retrieve_url(url, request_data=data)
+        except TypeError:
+            # Older versions of retrieve_url did not support request_data/POST, se we must do the
+            # request ourselves...
+            user_agent = 'Mozilla/5.0 (X11; Linux x86_64; rv:125.0) Gecko/20100101 Firefox/125.0'
+            req = urllib.request.Request(url, data, {'User-Agent': user_agent})
+            try:
+                response: http.client.HTTPResponse = urllib.request.urlopen(req)  # nosec B310 # pylint: disable=consider-using-with
+                return response.read().decode('utf-8')
+            except urllib.error.URLError as errno:
+                print(f"Connection error: {errno.reason}", file=sys.stderr)
+            return ""
 
     def search(self, what: str, cat: str = 'all') -> None:
-        # curr_cat = self.supported_categories[cat]
-        what = what.replace('%20', '+')
-        # analyze first 5 pages of results
-        for currPage in range(0, 5):
-            url = f"{self.url}/browse?t={what}&p={currPage}"
-            html = retrieve_url(url)
-            parser = self.MyHTMLParser(self.url)
-            parser.feed(html)
-            parser.close()
-            if len(parser.pageRes) < 20:
-                break
+        eztv_html = self.do_query(what)
 
-    def download_torrent(self, info: str) -> None:
-        """ Downloader """
-        html = retrieve_url(info)
-        m = re.search('href=[\'\"].*?(magnet.+?)[\'\"]', html)
-        if m and len(m.groups()) > 0:
-            magnet = unquote(m.group(1))
-            print(magnet + ' ' + info)
+        eztv_parser = self.MyHtmlParser(self.url)
+        eztv_parser.feed(eztv_html)
+        eztv_parser.close()
